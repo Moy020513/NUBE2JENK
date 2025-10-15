a39f49f1cd2f2cc1e5924a842ea3afce9679d168
MoisesMartinez1305 <l20590306@sjuanrio.tecnm.mx>
2025-10-14 18:26:18 -0600

    automatic

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app =  Flask(__name__)
 #Ruta raiz
 @app.route('/')
 def index():
-    return 'Hola Mundo, esto es una prueba 2 contenedor'
+    return 'Hola Mundo, esto es una prueba 2 contenedor, contenedor automatico cada minuto'
 
 if __name__ == '__main__':
     app.run(debug=True)
